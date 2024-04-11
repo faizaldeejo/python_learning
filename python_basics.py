d6cd94fbd489998132ceb995b220c10a856f9faf
faizal <syedfaizal905@gmail.com>
2024-04-11 20:50:45 +0530

    python basics

--- python_basics.py
+++ python_basics.py
@@ -1,1 +1,21 @@
-# 
+# print statment
+print("Hello World!")
+
+#Memory Address
+'''python takes same memory address, when two or more variables have same vales, id represents the address of the variable from memory'''
+var1 = 0
+var2 = 0
+print(id(var1), id(var2))
+
+#Delete variables in python
+'''del var1,var2
+print(id(var1), id(var2))'''
+
+#type() function
+'''By using python's build-in-function type(), we can know the data type of the variable'''
+var3 = 55
+var4 = 88.0
+var5 = "faizal"
+print(type(var3), type(var4), type(var5))
+
+
